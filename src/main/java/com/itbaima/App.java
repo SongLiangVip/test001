652dfe0eaee5b65abdb76fa058ae8a8cb63d7cda
余松亮 <@itheima.com>
2021-07-20 21:24:19 +0800

    test3

---
+++ src/main/java/com/itbaima/App.java
@@ -0,0 +1,13 @@
+package com.itbaima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
